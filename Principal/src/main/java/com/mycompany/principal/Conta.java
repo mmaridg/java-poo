49eecd11d6f65299d8fc5ce7454ad5b0ee83f552
Mariana Dias <mmaridg@gmail.com>
2026-08-12 22:35:16 -0300

    Melhorias no código da classe Conta e métodos
    Conserto dos cálculos e métodos da classe conta
    Agora, o código retorna corretamente o resultado das operações creditar, debitar e transferir valores entre as duas contas criadas c1 e c2.
    main

--- Principal/src/main/java/com/mycompany/principal/Conta.java
+++ Principal/src/main/java/com/mycompany/principal/Conta.java
@@ -4,7 +4,7 @@
  */
 package com.mycompany.principal;
 
-public class Conta {
+public class Conta { //criando a classe
     private int agencia, numero;
     private double saldo;
     
@@ -20,30 +20,62 @@ public class Conta {
     }
     
     void creditar (double valor){
-	saldo = saldo + valor;
+	if(valor > 0){
+            saldo = saldo + valor;
+
+        }
+        else {
+            System.out.println("Valor inválido.");
+        }
 }
 
     boolean debitar (double valor) {
-	if(valor <= saldo){
-			saldo = saldo - valor;
-			return true;
+	if(valor > 0 && valor <= saldo){
+            saldo = saldo - valor;
+
+            return true;
 		}
 	
 	else {
 		System.out.println("Saldo Insuficiente");
-
+            return false;
 	}
     }
 
 // A transferência fazemos 2 operções: debito da minha conta pra creditar em outra, porém para isso ocorrer, é preciso verificar primeiro se há saldo na conta, e se nÃO TIVER SALDO, a operação de crédito NÃO DEVE OCORRER.
 // Agora a transferência, usamos o método de débito já pronto.
 
-    void transferir (double valor, Conta destino){
+    void transferir (double valor, Conta destino){//criei uma variável do tipo Conta com o nome destino
 	if (debitar(valor)) {
-		destino.creditar(valor);
-	
+            destino.creditar(valor);
+
 	}
 	else {
             System.out.println("Transferência negada");
 	}
     }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
--- Principal/src/main/java/com/mycompany/principal/Principal.java
+++ Principal/src/main/java/com/mycompany/principal/Principal.java
@@ -3,24 +3,17 @@ package com.mycompany.principal;
 public class Principal {
       
     public static void main(String[] args) {
-        // Criando a conta 1 com 8000 reais
-        Conta c1 = new Conta(222, 898, 8000.00);
+        Conta c1 = new Conta(1, 1, 1000.00); // Criando a conta 1 com 1000 reais
+        Conta c2 = new Conta(1, 2, 800.00); // Criando a conta 2 com 890 reais
+       
+        // Operações
+        c1.creditar(500.00); 
+        c1.debitar(100.00);  
         
-        // Criando a conta 2 zerada
-        Conta c2 = new Conta(333, 999, 0.0);
+        // Transferência : A conta c1 está transferindo 200 reais para a c2
+        c1.transferir(200.00, c2); 
         
-        System.out.println("Saldo inicial C1: " + c1.getSaldo());
-        System.out.println("Saldo inicial C2: " + c2.getSaldo());
-                
-        // Operações na Conta 1
-        c1.creditar(500.00); // Adiciona 500
-        c1.debitar(100.00);  // Tira 100
-        
-        // Transferência : A conta c1 está transferindo 400 reais para a c2
-        c1.transferir(400.00, c2); 
-        
-        System.out.println("-------------------------");
-        System.out.println("Saldo final C1: " + c1.getSaldo());
-        System.out.println("Saldo final C2: " + c2.getSaldo());
+        System.out.println("Saldo conta C1: " + c1.getSaldo());
+        System.out.println("Saldo conta C2: " + c2.getSaldo());
     }
 }
